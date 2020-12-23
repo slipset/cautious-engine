ada0eeeb28fca8c0e62db8da98539626c5218567
Erik Assum <erik@assum.net>
2020-12-23 17:39:02 +0100

    First commit

---
+++ test/cautious_engine/core_test.clj
@@ -0,0 +1,7 @@
+(ns cautious-engine.core-test
+  (:require [clojure.test :refer :all]
+            [cautious-engine.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
